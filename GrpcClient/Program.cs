634a5271bb921a849795e4c322b64bf54443f8d9
641489189@qq.com <641489189@qq.com>
2020-08-10 00:14:11 +0800

    init code

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using GrpcServer.Proto;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+      using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+      var client = new GetEmployee.GetEmployeeClient(channel);
+
+      var response = await client.GetByNoAsync(new GetByNoRequest
+      {
+        No = 1
+      });
+
+
+
+
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ GrpcServer1/Services/MyEmployeeService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using GrpcServer.Proto;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer1.Services
+{
+  public class MyEmployeeService:GetEmployee.GetEmployeeBase
+  {
+    private readonly ILogger<MyEmployeeService> _logger;
+    public MyEmployeeService(ILogger<MyEmployeeService> logger)
+    {
+      _logger = logger;
+    }
+    public override Task<GetResponse> GetByNo(GetByNoRequest request, ServerCallContext context)
+    {
+      return Task.FromResult(new GetResponse()
+      {
+        Employee = new Employee() {Id=1,No=1,Name="1213" }
+      });
+    }
+
+
+
+  }
+}
